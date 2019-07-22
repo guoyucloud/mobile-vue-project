11f932748992560780c7968bb23f2b4f8de29434
guoyu <120098866@qq.com>
2019-07-22 14:30:52 +0800

    配置好eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,42 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		'plugin:vue/strongly-recommended',
+		'@vue/standard'
+	],
+	rules: {
+		// allow async-await
+		'generator-star-spacing': 'off',
+		// 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-useless-escape': 'off',
+		'indent': ['error', 'tab'],
+		'no-tabs': 'off',
+		'vue/html-indent': ['error', 'tab'],
+		'vue/component-name-in-template-casing': 'off',
+		'vue/max-attributes-per-line': ['warn', {
+			'singleline': 3,
+			'multiline': {
+				'max': 1,
+				'allowFirstLine': false
+			}
+		}]
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	overrides: [
+		{
+			files: ['*.vue'],
+			rules: {
+				indent: 'off'
+			}
+		}
+	],
+	plugins: [
+		'vue'
+	]
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+	</div>
 </template>
 
 <script>
@@ -10,7 +10,7 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
+  name: 'Home',
   components: {
     HelloWorld
   }
